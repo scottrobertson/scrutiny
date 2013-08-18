231e33fec91cb8a82acfc41785486cb2bb070e19
Scott Robertson <scottymeuk@gmail.com>
2013-08-18 11:05:36 +0100

    Add testing framework and some basic tests

---
+++ tests/ScottRobertson/Scrutiny/Test/ClientTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ScottRobertson\Scrutiny\Test;
+
+class ClientTest extends TestCase
+{
+    public function testInit()
+    {
+        $client = new \ScottRobertson\Scrutiny\Client();
+        $this->assertTrue(is_object($client));
+        $this->assertCount(0, $client->getServices());
+        $this->assertCount(0, $client->getReporters());
+    }
+
+    public function testAddService()
+    {
+        $client = new \ScottRobertson\Scrutiny\Client();
+        $client->addService(new \ScottRobertson\Scrutiny\Service\Url('http://google.com'));
+        $this->assertCount(1, $client->getServices());
+    }
+
+    public function testAddReporter()
+    {
+        $client = new \ScottRobertson\Scrutiny\Client();
+        $client->addReporter(new \ScottRobertson\Scrutiny\Reporter\Null());
+        $this->assertCount(1, $client->getReporters());
+    }
+}
---
+++ tests/ScottRobertson/Scrutiny/Test/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ScottRobertson\Scrutiny\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
