f5dc384aa397c529db74114f70c277ed9622df19
Scott Robertson <scottymeuk@gmail.com>
2013-08-19 23:02:21 +0100

    Add a generic Post reporter

---
+++ src/ScottRobertson/Scrutiny/Reporter/Post.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ScottRobertson\Scrutiny\Reporter;
+
+class Post extends Base
+{
+    protected $url;
+    protected $curl;
+
+    public function __construct($url, array $events = array('down'))
+    {
+        $this->url = $url;
+        $this->subscribe($events);
+        $this->curl = curl_init($this->url);
+    }
+
+    public function report($service)
+    {
+        return $this->post(
+            array(
+                'title' => $service->getMessageFromEvent(),
+                'message' => $service->getName() . ' returned ' . $service->getData('code'),
+                'timestamp' => $service->getTime(),
+                'url' => $service->getData('url')
+            )
+        );
+    }
+
+    private function post($data)
+    {
+        curl_setopt_array(
+            $this->curl,
+            array(
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => http_build_query($data)
+            )
+        );
+
+        $response = curl_exec($this->curl);
+        return $response;
+    }
+
+    public function __destruct()
+    {
+        curl_close($this->curl);
+    }
+}
