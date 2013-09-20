ecdaf9a21651964c28f0d56c83ba1ffe6466dba1
Scott Robertson <scottymeuk@gmail.com>
2013-09-20 09:58:04 +0100

    Comments

--- src/ScottRobertson/Scrutiny/Reporter/Base.php
+++ src/ScottRobertson/Scrutiny/Reporter/Base.php
@@ -4,13 +4,29 @@ namespace ScottRobertson\Scrutiny\Reporter;
 
 class Base
 {
-    private $subscriptions = array('down', 'recovery');
+    /**
+     * Holds an array of what events the reporter is subscribed to
+     * @var array
+     */
+    private $subscriptions = array(
+        'down',
+        'recovery'
+    );
 
+    /**
+     * Check to see if the reporter is subscribed to an event
+     * @param  string $event
+     * @return bool
+     */
     public function subscribed($event)
     {
         return in_array($event, $this->subscriptions);
     }
 
+    /**
+     * Subscribe the reporter to an event
+     * @param  string $events
+     */
     public function subscribe($events)
     {
         $this->subscriptions = $events;
