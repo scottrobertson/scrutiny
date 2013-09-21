a68efd75a14f580e80d52fc9461414ac5f48ba48
Scott Robertson <scott@scottrobertson.me>
2013-09-21 18:24:51 +0100

    Comments

--- src/ScottRobertson/Scrutiny/Client.php
+++ src/ScottRobertson/Scrutiny/Client.php
@@ -4,9 +4,22 @@ namespace ScottRobertson\Scrutiny;
 
 class Client
 {
+    /**
+     * Array of services we wish to watch
+     * @var array
+     */
     protected $services = array();
+
+    /**
+     * Array of reporters we wish to send responses
+     * @var array
+     */
     protected $reporters = array();
 
+    /**
+     * Add a service to our internal array
+     * @param Object $service
+     */
     public function addService($service)
     {
         if (! method_exists($service, 'getStatus')) {
@@ -16,6 +29,10 @@ class Client
         $this->services[] = $service;
     }
 
+    /**
+     * Add a reporter to our internal array
+     * @param Object $reporter
+     */
     public function addReporter($reporter)
     {
         if (! method_exists($reporter, 'report')) {
@@ -25,16 +42,27 @@ class Client
         $this->reporters[] = $reporter;
     }
 
+    /**
+     * Returns an array of all current services
+     * @return array
+     */
     public function getServices()
     {
         return $this->services;
     }
 
+    /**
+     * Returns an array of all current reporters
+     * @return array
+     */
     public function getReporters()
     {
         return $this->reporters;
     }
 
+    /**
+     * This is the main method to watch all our services.
+     */
     public function watch()
     {
         while (true) {
@@ -49,6 +77,10 @@ class Client
         }
     }
 
+    /**
+     * Report the status of our service to each of our reporters
+     * @param  ScottRobertsonScrutinyServiceBase $service
+     */
     public function report(\ScottRobertson\Scrutiny\Service\Base $service)
     {
         foreach ($this->reporters as $report) {
