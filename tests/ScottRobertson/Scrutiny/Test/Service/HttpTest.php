43cb4b1da381c13c3a6473d7fb4330a29d42f2e8
Scott Robertson <scott@scottrobertson.me>
2013-10-06 20:48:41 +0100

    Simple test for http service. Not sure how to mock this out really.

---
+++ tests/ScottRobertson/Scrutiny/Test/Service/HttpTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ScottRobertson\Scrutiny\Test\Service;
+
+class HttpTest extends \ScottRobertson\Scrutiny\Test\TestCase
+{
+    public function testInit()
+    {
+        $http = new \ScottRobertson\Scrutiny\Service\Http('http://127.0.0.1');
+        $this->assertTrue(is_object($http));
+        $this->assertTrue(method_exists($http, 'checkable'));
+        $this->assertTrue(method_exists($http, 'getStatus'));
+        $this->assertTrue(method_exists($http, 'isOnline'));
+
+        $this->assertTrue($http->getStatus());
+    }
+}
